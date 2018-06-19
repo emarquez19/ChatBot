35ab85d59022123a6317389bd5ed3cc2c26ae542
emarquez19 <edwin.marquez@digital-fintech.com>
2018-06-19 15:29:52 -0500

    cambios hoy

--- js/index.js
+++ js/index.js
@@ -36,6 +36,7 @@ $(function() {
     str += "        <\/div>";
     $(".chat-logs").append(str);
     $("#cm-msg-"+INDEX).hide().fadeIn(300);
+    // $("#cm-msg-"+INDEX).hide().addClass('bounce');
     if(type == 'self'){
      $("#chat-input").val(''); 
     }    
@@ -43,18 +44,7 @@ $(function() {
   }  
   
   function generate_button_message(msg, buttons){    
-    /* Buttons should be object array 
-      [
-        {
-          name: 'Existing User',
-          value: 'existing'
-        },
-        {
-          name: 'New User',
-          value: 'new'
-        }
-      ]
-    */
+
     INDEX++;
     var btn_obj = buttons.map(function(button) {
        return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
@@ -78,21 +68,96 @@ $(function() {
     $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
     $("#chat-input").attr("disabled", true);
   }
-  
+
   $(document).delegate(".chat-btn", "click", function() {
     var value = $(this).attr("chat-value");
     var name = $(this).html();
     $("#chat-input").attr("disabled", false);
     generate_message(name, 'self');
-  })
+  });
   
+  // Abrir y cerrar el chat
   $("#chat-circle").click(function() {    
     $("#chat-circle").toggle('scale');
     $(".chat-box").toggle('scale');
-  })
+  });
   
   $(".chat-box-toggle").click(function() {
     $("#chat-circle").toggle('scale');
     $(".chat-box").toggle('scale');
-  }) 
+  });
+
+  // Tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+
+  var chatbox = $("#chat-box");
+  
+  // Alinear a la Izquierda
+  $("#pin-left").click(function() {
+    clearPositionLog(1);
+    if ( $(this).hasClass('active')) {
+      clearActive();
+      chatbox.removeClass('left-pinned').addClass('back-to-origin');
+    } else {
+      clearActive();
+      $(this).addClass('active');
+      chatbox.addClass('left-pinned');
+    }
+  });
+
+  // Alinear a la Derecha
+  $("#pin-right").click(function() {
+    clearPositionLog(2);
+    if ( $(this).hasClass('active')) {
+      clearActive();
+      chatbox.removeClass('right-pinned').addClass('back-to-origin');
+    } else {
+      clearActive();
+      $(this).addClass('active');
+      chatbox.addClass('right-pinned');
+    }
+  });
+
+  // Posicion Libre
+  // $("#position-free").click(function() {
+  //   clearPositionLog(3);
+  //   $("#chat-box").toggleClass('draggable-box');
+  //   setTimeout (function (){
+  //     Draggable.create('.draggable-box');
+  //   },100);
+  // });
+
+  // Pantalla Completa
+  $("#expand-box").click(function() {
+    clearPositionLog(4);
+    if ( $(this).hasClass('active')) {
+      clearActive();
+      chatbox.removeClass('full-screen').addClass('back-to-origin');
+    } else {
+      clearActive();
+      $(this).addClass('active');
+      chatbox.addClass('full-screen');
+    }
+  });
+
+  function clearPositionLog(i) {
+    if (i == 1){
+      // Draggable.get('.draggable-box').disable();
+      $("#chat-box").removeClass('right-pinned draggable-box full-screen back-to-origin');
+    } else if (i == 2){
+      $("#chat-box").removeClass('left-pinned draggable-box full-screen back-to-origin');
+    } else if (i == 3) {
+      $("#chat-box").removeClass('left-pinned right-pinned full-screen back-to-origin');
+    } else if (i == 4) {
+      $("#chat-box").removeClass('left-pinned right-pinned draggable-box back-to-origin');
+    } else {
+      // alert("Ninguno");
+    }
+  }
+
+  function clearActive() {
+    $('.chat-options span').removeClass('active');
+  }
+
 });
